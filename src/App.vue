8c796165ea8a3036c7d777f7e990b761897eaecb
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2022-09-04 19:04:36 +0200

    Front Home page

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,43 @@ body{
   font-family: Poppins, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: $black;
-  color: White;
+  background: $background;
+  color: $main-blue;
+  height: 100vh;
+
+  h1{
+    margin: 0;
+    position: relative;
+
+    &::before{
+      content: '';
+      position: absolute;
+      width: 80%;
+      height: 1px;
+      bottom: 50px;
+      left: 10%;
+      background: $second-blue;
+    }
+  }
+  .main_button{
+    font-family: Poppins, sans-serif;
+    cursor: pointer;
+    background: $main-blue;
+    text-align: center;
+    color: white;
+    border: none;
+    padding: 15px 0;
+    width: 55%;
+    min-width: 231px;
+    font-weight: 600;
+    font-size: 16px;
+    box-shadow: 0px 20px 30px rgba(10, 177, 187, 0.26);
+    border-radius: 10px;
+    transition: all 200ms linear;
+
+    &:hover{
+      background: #07858b;
+    }
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    INIT
-    <img src="@/assets/img/logo.png" alt="">
+    <img id="bonnet_image" src="@/assets/img/bonnet.png" alt="Bonnet">
+    <img id="hoodie_image" src="@/assets/img/hoodie.png" alt="Hoodie">
+    <h1>Bienvenue 👋</h1>
+    <button id="button_top" class="main_button">Créer un partie</button>
+    <button class="main_button">Rejoindre un partie</button>
   </div>
 </template>
 
@@ -11,3 +14,39 @@ export default {
   name: 'HomeView',
 }
 </script>
+
+<style lang="scss" scopped>
+
+.home{
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0px;
+
+  #hoodie_image{
+    position: absolute;
+    left: -100px;
+    bottom: -40px;
+    animation: float 6s ease-in-out infinite;
+  }
+
+  #bonnet_image{
+    position: absolute;
+    right: 50px;
+    top: 70px;
+    animation: float 6s 1s ease-in-out infinite;
+  }
+
+  h1{
+    font-weight: 700;
+    font-size: 40px;
+    padding-bottom: 50px;
+  }
+
+  #button_top{
+    margin-bottom: 25px;
+  }
+}
+</style>
