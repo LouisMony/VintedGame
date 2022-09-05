dfc5f996594c6057500cffec7b142cbb75122f5c
Louis Mony <73167166+LouisMony@users.noreply.github.com>
2022-09-05 09:35:39 +0200

    Update FavIco

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    themeColor: '#17a2b8',
+    msTileColor: '#000000',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 })
